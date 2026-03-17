fe403899a1b9adf03fb1c4153ecff420eb18fe16
Evgeniy <egornyak@gmail.com>
2026-03-17 20:18:56 +0200

    fix shared database

--- database.py
+++ database.py
@@ -6,37 +6,8 @@ cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS transactions(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
 type TEXT,
 category TEXT,
-amount REAL,
-date TEXT
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS jars(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-name TEXT,
-amount REAL
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS debts(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-person TEXT,
-amount REAL
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS installments(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-name TEXT,
 amount REAL,
 date TEXT
 )
@@ -45,11 +16,11 @@ date TEXT
 conn.commit()
 
 
-def add_transaction(user_id,type_,category,amount,date):
+def add_transaction(user_id, type_, category, amount, date):
 
     cursor.execute(
-    "INSERT INTO transactions(user_id,type,category,amount,date) VALUES(?,?,?,?,?)",
-    (user_id,type_,category,amount,date)
+    "INSERT INTO transactions(type,category,amount,date) VALUES(?,?,?,?)",
+    (type_,category,amount,date)
     )
 
     conn.commit()
@@ -57,22 +28,19 @@ def add_transaction(user_id,type_,category,amount,date):
 
 def get_balance(user_id):
 
-    cursor.execute(
-    "SELECT type,amount FROM transactions WHERE user_id=?",
-    (user_id,)
-    )
+    cursor.execute("SELECT type,amount FROM transactions")
 
-    rows=cursor.fetchall()
+    rows = cursor.fetchall()
 
-    balance=0
+    balance = 0
 
     for r in rows:
 
-        if r[0]=="income":
-            balance+=r[1]
+        if r[0] == "income":
+            balance += r[1]
 
-        elif r[0]=="expense":
-            balance-=r[1]
+        elif r[0] == "expense":
+            balance -= r[1]
 
     return balance
 
@@ -80,8 +48,7 @@ def get_balance(user_id):
 def get_last_transactions(user_id):
 
     cursor.execute(
-    "SELECT type,category,amount,date FROM transactions WHERE user_id=? ORDER BY id DESC LIMIT 10",
-    (user_id,)
+    "SELECT type,category,amount,date FROM transactions ORDER BY id DESC LIMIT 10"
     )
 
     return cursor.fetchall()
